6f82690e4befa1508ab169030053e838616f6c04
IkerEAE <158267708+IkerEAE@users.noreply.github.com>
2024-02-10 11:52:27 +0100

    Update YearSalary.vue

--- resources/js/Pages/Components/YearSalary.vue
+++ resources/js/Pages/Components/YearSalary.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="bg-white shadow-md border border-gray-200 py-4 px-4 rounded-lg">
         <h3 class="pb-3">Year</h3>
-        <DoughnutChart :chartData="chartData" :options="options" />
+        <LineChart :chartData="chartData" :options="options" />
     </div>
 </template>
 
 <script>
 import { defineComponent, onMounted, ref } from 'vue';
-import { DoughnutChart } from 'vue-chart-3'; 
+import { LineChart } from 'vue-chart-3'; 
 import { Chart, registerables } from "chart.js";
 import axios from 'axios';
 
 Chart.register(...registerables);
 
 export default defineComponent({
-    name: 'Position',
-    components: { DoughnutChart }, 
+    name: 'YearChart', // Cambiar el nombre del componente
+    components: { LineChart }, 
     setup() {
         const chartData = ref({});
 
@@ -30,8 +30,10 @@ export default defineComponent({
                     labels: labels,
                     datasets: [
                         {
+                            label: 'Average Salary', // Etiqueta del conjunto de datos
                             data: values,
-                            backgroundColor: ['#77CEFF', '#0079AF', '#123E6B']
+                            borderColor: 'blue', // Color de la línea
+                            fill: false // No rellenar debajo de la línea
                         },
                     ],
                 }
