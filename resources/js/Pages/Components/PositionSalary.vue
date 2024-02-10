58fde2b3725ddd08869f834179d1bb075c14ef9a
IkerEAE <158267708+IkerEAE@users.noreply.github.com>
2024-02-10 11:16:16 +0100

    Update PositionSalary.vue

--- resources/js/Pages/Components/PositionSalary.vue
+++ resources/js/Pages/Components/PositionSalary.vue
@@ -1,32 +1,60 @@
 <template>
     <div class="bg-white shadow-md border border-gray-200 py-4 px-4 rounded-lg">
-        <h3 class="pb-3">Salario / Posición</h3>
-    <DoughnutChart :chartData="testData" />
+        <h3 class="pb-3">Position</h3>
+        <PieChart :chartData="chartData" :options="options"  />
     </div>
-  </template>
+</template>
 
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  import { DoughnutChart } from 'vue-chart-3';
-  import { Chart, registerables } from "chart.js";
+<script>
+import { defineComponent, onMounted, ref } from 'vue';
+import { PieChart } from 'vue-chart-3';
+import { Chart, registerables } from "chart.js";
+import axios from 'axios';
 
-  Chart.register(...registerables);
+Chart.register(...registerables);
 
-  export default defineComponent({
-    name: 'PositionSalary',
-    components: { DoughnutChart },
+export default defineComponent({
+    name: 'Position',
+    components: { PieChart },
     setup() {
-      const testData = {
-        labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
-        datasets: [
-          {
-            data: [30, 40, 60, 70, 5],
-            backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
-          },
-        ],
-      };
 
-      return { testData };
+        const chartData = ref({});
+
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://48fk51szmi.execute-api.us-east-1.amazonaws.com/dev/salaries/position');
+                const data = JSON.parse(response.data.data);
+                const labels = data.map(item => item.position_type);
+                const values = data.map(item => item.position_count);
+
+                chartData.value = {
+                    labels: labels,
+                    datasets: [
+                        {
+                            data: values,
+                            backgroundColor: ['#77CEFF', '#0079AF', '#123E6B']
+                        },
+                    ],
+                }
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        onMounted(() => {
+            fetchData();
+        });
+
+        const options = {
+            plugins: {
+                legend: {
+                    display: true
+                }
+            }
+        }
+
+        return { chartData, options };
     },
-  });
-  </script>
+});
+</script>
