cef10311788cd69563f616fb79031c1c7ebfaa39
IkerEAE <158267708+IkerEAE@users.noreply.github.com>
2024-02-10 11:50:39 +0100

    Update YearSalary.vue

--- resources/js/Pages/Components/YearSalary.vue
+++ resources/js/Pages/Components/YearSalary.vue
@@ -1,41 +1,59 @@
 <template>
     <div class="bg-white shadow-md border border-gray-200 py-4 px-4 rounded-lg">
-        <h3 class="pb-3">Salarios / Year</h3>
-    <LineChart :chartData="testData" :options="options" />
+        <h3 class="pb-3">Year</h3>
+        <DoughnutChart :chartData="chartData" :options="options" />
     </div>
 </template>
-  
+
 <script>
-import { defineComponent } from 'vue';
-import { LineChart } from 'vue-chart-3';
+import { defineComponent, onMounted, ref } from 'vue';
+import { DoughnutChart } from 'vue-chart-3'; 
 import { Chart, registerables } from "chart.js";
+import axios from 'axios';
 
 Chart.register(...registerables);
 
 export default defineComponent({
-    name: 'Home',
-    components: { LineChart },
+    name: 'Position',
+    components: { DoughnutChart }, 
     setup() {
-        const testData = {
-            labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
+        const chartData = ref({});
 
-            datasets: [
-                {
-                    data: [30, 40, 60, 70, 5],
-                    backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
-                },
-            ],
-        };
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://48fk51szmi.execute-api.us-east-1.amazonaws.com/dev/salaries/year');
+                const data = JSON.parse(response.data.data);
+                const labels = data.map(item => item.work_year);
+                const values = data.map(item => item.average_salary);
+
+                chartData.value = {
+                    labels: labels,
+                    datasets: [
+                        {
+                            data: values,
+                            backgroundColor: ['#77CEFF', '#0079AF', '#123E6B']
+                        },
+                    ],
+                }
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        onMounted(() => {
+            fetchData();
+        });
 
         const options = {
             plugins: {
                 legend: {
-                    display: false
+                    display: true
                 }
             }
         }
 
-        return { testData, options };
+        return { chartData, options };
     },
 });
 </script>
